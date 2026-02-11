248b23888e24d55a1a84a7ec32430b388a8ad09c
stuNero <max.vemic@student.nbi-handelsakademin.se>
2026-02-11 15:32:35 +0100

    fixed main to middle of screen + fit entire screen

--- src/partials/Main.tsx
+++ src/partials/Main.tsx
@@ -1,7 +1,7 @@
 import { Outlet } from 'react-router-dom';
 
 export default function Main() {
-  return <main className='h-[100vh]'>
+  return <main className='h-[100vh] flex flex-row justify-center container max-w-7xl mx-auto'>
     <div>
       <Outlet />
     </div>
