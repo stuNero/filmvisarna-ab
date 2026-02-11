03daa030864f1dff69a3e672f6d3179094d3a114
stuNero <max.vemic@student.nbi-handelsakademin.se>
2026-02-11 15:33:47 +0100

    fixed background size, fixed homepage content pos

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -6,12 +6,20 @@ HomePage.route = {
 
 export default function HomePage() {
   return <>
-    <section>
-      <h2>Välkommen!</h2>
-      <p></p>
+    <section id="hero" className="flex flex-col">
+      <div className="mt-24">
+        <h1 className="">Upplev bio som aldrig förr</h1>
+        <pre>
+          Fördjupa dig i de senaste storfilmerna med toppmodern ljudteknik och fantastisk
+          bild. Boka dina biljetter nu.
+        </pre>
+      </div>
     </section>
     <section>
-
+      <div >
+        <h2>Visas Nu</h2>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quo eius officia illo in non quam omnis quas dolores tenetur cum quaerat reiciendis id, provident voluptatum natus. Blanditiis, obcaecati expedita! Molestias?</p>
+      </div>
     </section>
   </>;
 }
