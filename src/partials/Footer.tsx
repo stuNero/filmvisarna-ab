3cdeb8a4b626fc08e78d14d636bbddcf872cd0b9
stuNero <max.vemic@student.nbi-handelsakademin.se>
2026-02-11 15:31:31 +0100

    add homepage link to footer + styling

--- src/partials/Footer.tsx
+++ src/partials/Footer.tsx
@@ -1,7 +1,14 @@
+import { Link, useLocation } from 'react-router-dom';
+
 export default function Footer() {
-  return <footer className="relative bg-opacity-20">
-    <div>
-      © CineSharp {new Date().getFullYear()}
+  return <footer className="relative bg-black border-t-2 border-solid border-white h-[200px]">
+    <div className='max-w-7xl mx-auto'>
+      <Link to="/" className='hover:text-red-800 transition-colors duration-400 mt-[50px] flex flex-row w-fit'>
+        < img src="/logo-cinesharp.webp" width="50px" height="auto" ></img >
+        <h1 className='font-bold content-center pl-[10px] '>
+          CineSharp
+        </h1>
+      </Link >
     </div>
   </footer>;
 }
