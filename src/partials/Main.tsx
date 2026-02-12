e490a9a886475d2725fb26be57e60be451f7e4e4
stuNero <max.vemic@student.nbi-handelsakademin.se>
2026-02-12 11:25:13 +0100

    fixed text positioning on main content

--- src/partials/Main.tsx
+++ src/partials/Main.tsx
@@ -1,9 +1,7 @@
 import { Outlet } from 'react-router-dom';
 
 export default function Main() {
-  return <main className='h-[100vh] flex flex-row justify-center container max-w-7xl mx-auto'>
-    <div>
-      <Outlet />
-    </div>
+  return <main className='h-[100vh] flex flex-col justify-center content-center container max-w-7xl mx-auto'>
+    <Outlet />
   </main>;
 }
