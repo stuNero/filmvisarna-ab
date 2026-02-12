162653660dd92f5606a3a0889bf1db202798fa36
Piercor <pierinocr@gmail.com>
2026-02-12 12:50:25 +0100

    add/ HTML elements with ids to change CSS attributes

--- src/pages/NotFoundPage.tsx
+++ src/pages/NotFoundPage.tsx
@@ -6,11 +6,15 @@ NotFoundPage.route = {
 
 export default function NotFoundPage() {
   return <>
-    <h2>Inte hittad: 404</h2>
-    <p>
-      Vi ber om ursäkt det verkar inte finnas något på denna sidan som matchar din url:
-    </p>
-    <p><strong>{useLocation().pathname.slice(1)}</strong></p>
-    <p><Link to="/">Gå till start sidan</Link> istället.</p>
+    <section id="notFoundCtn" className="flex flex-col max-w-7xl">
+      <div id="notFoundTxt">
+        <h2>Inte hittad: 404</h2><br />
+        <p>
+          Vi ber om ursäkt det verkar inte finnas något på denna sidan som matchar din url:
+        </p><br />
+        <p><strong>{useLocation().pathname.slice(1)}</strong></p><br />
+        <p id="notFoundLinkTxt"><Link to="/">Gå till start sidan</Link> istället.</p>
+      </div>
+    </section>
   </>;
 }
