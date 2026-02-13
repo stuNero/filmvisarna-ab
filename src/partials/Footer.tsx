560fe28862cd85feb633eb0ad310149bc2e5967d
stuNero <max.vemic@student.nbi-handelsakademin.se>
2026-02-13 11:18:33 +0100

    add nav links to support and locale information

--- src/partials/Footer.tsx
+++ src/partials/Footer.tsx
@@ -1,14 +1,33 @@
-import { Link, useLocation } from 'react-router-dom';
+import { Link } from 'react-router-dom';
+
+import LocaleDetailsPage from '../pages/LocaleDetailsPage';
+import KioskInfoPage from '../pages/KioskPage';
+import CancelBookingPage from '../pages/CancelBookingPage';
 
 export default function Footer() {
-  return <footer className="relative bg-black border-t-2 border-solid border-white h-[200px]">
-    <div className='max-w-7xl mx-auto'>
-      <Link to="/" className='hover:text-red-800 transition-colors duration-400 mt-[50px] flex flex-row w-fit'>
+  return <footer className="w-full flex flex-row relative bg-black border-t-2 border-solid border-stone-600 h-[200px] container max-w-7xl mx-auto">
+
+    <article className='flex flex-col mt-[25px] px-[50px]'>
+      <Link to="/" className='flex flex-row hover:text-red-800 transition-colors duration-400 mb-[10px]'>
         < img src="/logo-cinesharp.webp" width="50px" height="auto" ></img >
         <h1 className='font-bold content-center pl-[10px] '>
           CineSharp
         </h1>
       </Link >
-    </div>
+      <p className='opacity-50'>Din ultimata destination för de senaste filmerna<br /> och oförglömliga bioupplevelser.</p>
+    </article>
+    <article className='flex flex-col mt-[25px] px-[50px]'>
+      <h1 className='text-lg'>Snabblänkar:</h1>
+      <Link to={LocaleDetailsPage.route.path} className='opacity-50 hover:opacity-100 hover:text-red-800 transition-colors duration-250'>
+        {LocaleDetailsPage.route.menuLabel}</Link>
+      <Link to={KioskInfoPage.route.path} className='opacity-50 hover:opacity-100 hover:text-red-800 transition-colors duration-250'>
+        {KioskInfoPage.route.menuLabel}</Link>
+    </article>
+    <article className='flex flex-col mt-[25px] px-[50px]'>
+      <h1 className='text-lg'>Support:</h1>
+      <Link to={CancelBookingPage.route.path} className='opacity-50 hover:opacity-100 hover:text-red-800 transition-colors duration-250'>
+        {CancelBookingPage.route.menuLabel}</Link>
+    </article>
+
   </footer>;
 }
