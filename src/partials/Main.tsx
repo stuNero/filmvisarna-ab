59ade80ed879416983a073fe5b18305c3136a0c8
Piercor <pierinocr@gmail.com>
2026-02-19 09:32:55 +0100

    fix/ TW properties for height
    The home page wasn't visilizing as it should before the change.

--- src/partials/Main.tsx
+++ src/partials/Main.tsx
@@ -1,7 +1,7 @@
 import { Outlet } from 'react-router-dom';
 
 export default function Main() {
-  return <main className='h-[100vh] flex flex-col justify-center content-center container max-w-7xl mx-auto'>
+  return <main className='min-h-full scroll-smooth flex flex-col justify-center content-center container max-w-7xl mx-auto'>
     <Outlet />
   </main>;
 }
