ec172fc74c7e9d6e74a49fdb44b0667bada4355a
Piercor <pierinocr@gmail.com>
2026-02-19 09:36:25 +0100

    add/ custom hook to fetch data from the API.

---
+++ src/utils/useFetchJson.ts
@@ -0,0 +1,19 @@
+
+import { useState, useEffect } from "react";
+import fetchJson from "./fetchJson";
+
+export default function useFetchJson<Type>(url: string) {
+
+  const [data, setData] = useState<Type | null>(null);
+
+  useEffect(() => {
+    (async () => {
+      setData(await fetchJson(url));
+    })();
+  }, []);
+
+  async function reloadData() {
+    setData(await fetchJson(url));
+  }
+  return [data, reloadData] as const;
+}
