ad739177c354a713e75ba6b1ebd934970c3e14d2
stuNero <max.vemic@student.nbi-handelsakademin.se>
2026-02-19 10:35:45 +0100

    add layout styling to each page
    to fix displaying content of the page correctly

--- src/pages/CancelBookingPage.tsx
+++ src/pages/CancelBookingPage.tsx
@@ -6,7 +6,8 @@ CancelBookingPage.route = {
 
 export default function CancelBookingPage() {
   return <>
-    <div>
+    <div className="                        top-0 bottom-0 left-0 h-[75vh]
+                        content-center justify-center">
       <h2>Detta är sidan för att avboka</h2>
       <p></p>
     </div>
