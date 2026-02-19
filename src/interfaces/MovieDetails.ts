501a0d6a135d30d65a63b5df79fbdb04221c9a4a
Piercor <pierinocr@gmail.com>
2026-02-19 08:56:24 +0100

    add/ MovieDetails interface.

---
+++ src/interfaces/MovieDetails.ts
@@ -0,0 +1,15 @@
+
+export interface MovieDetails {
+  id: number,
+  title: string,
+  productionYear: number,
+  lenght: number,
+  genre: string;
+  distributor: string,
+  audio: string,
+  subtitles: string,
+  director: string,
+  description: string,
+  youtube: string,
+  coverImage: string;
+}
