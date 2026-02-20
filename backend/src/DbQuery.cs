f25093beed45be69df789b71a1a0162cb0561317
stuNero <max.vemic@student.nbi-handelsakademin.se>
2026-02-20 11:33:49 +0100

    add views & fixed various columns

--- backend/src/DbQuery.cs
+++ backend/src/DbQuery.cs
@@ -131,9 +131,9 @@ public static class DbQuery
             );
 
             CREATE TABLE IF NOT EXISTS bookings (
-                id INT PRIMARY KEY UNIQUE,
+                id INT PRIMARY KEY,
                 cost INT NOT NULL,
-                createdAt TIMESTAMP NOT NULL,
+                createdAt TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
                 showingId INT NOT NULL,
                 FOREIGN KEY (showingId) REFERENCES showings(id)
             );
@@ -163,7 +163,34 @@ public static class DbQuery
                 email VARCHAR(255) NOT NULL,
                 FOREIGN KEY (bookingId) REFERENCES bookings(id)
             );
+            CREATE VIEW movieShowings AS
+                SELECT f.title,
+                    s.id AS showingId, s.timeSlot, s.venueID,
+                    v.name
+                FROM films f,
+                    showings s,
+                    venues v
+                WHERE f.id = s.filmId
+                AND s.venueID = v.id
+            ;
 
+            CREATE VIEW bookedSeatsWithShowings AS
+                SELECT bs.seatId, bs.bookingId, bs.ticketType,
+                    b.showingId,
+                    s.rowNr, s.columnNr
+                FROM bookedSeat bs,
+                    bookings b,
+                    seats s
+                WHERE bs.bookingId = b.id AND s.id = bs.seatId
+            ;
+
+            CREATE VIEW showingsWithOccupiedSeats AS
+                SELECT movieShowings.*,
+                    rowNr,columnNr
+                FROM movieShowings,
+                    bookedSeatsWithShowings
+                WHERE movieShowings.showingId = bookedSeatsWithShowings.showingId
+            ;
         ";
 
         // Execute each statement separately
