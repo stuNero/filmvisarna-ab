84a1e9eb8a24fb579b3075ac0faf9085a3dc8909
jacoblasse <jacoblasse4@gmail.com>
2026-02-20 14:35:31 +0100

    added div box for booking number and seat details

--- src/pages/BookingConfirmationPage.tsx
+++ src/pages/BookingConfirmationPage.tsx
@@ -7,19 +7,25 @@ BookingConfirmationPage.route = {
 export default function BookingConfirmationPage() {
   return (
     <div className="min-h-[75vh] flex items-center justify-center">
-      <div className="bg-zinc-950 rounded-lg max-w-md w-full mx-4 shadow-lg border border-zinc-700 overflow-hidden">
+      <div className="bg-zinc-950 rounded-lg max-w-3xl w-full shadow-lg border border-zinc-700 overflow-hidden flex flex-col">
         <div className="bg-red-800 p-6 text-center">
           <h2 className="text-2xl font-bold text-white mb-2">Bokning bekräftad!</h2>
           <p className="text-white/90">Tack för att du väljer vår bio. Dina biljetter är klara.</p>
         </div>
-        
-        <div className="p-8">
-          <div className="md p-4 ">
-            <h3 className="text-sm font-semibold text-gray-500 mb-2">Filmdetaljer</h3>
-            <div className="text-2xl font-bold text-white mb-2">Dune 2</div>
+        <div className="p-8 flex flex-row gap-6">
+          <div className="flex-1 flex flex-col gap-4">
+            <div className="md p-4 ">
+              <h3 className="text-sm font-semibold text-gray-500 mb-2">Filmdetaljer</h3>
+              <div className="text-2xl font-bold text-white mb-2">Dune 2</div>
+            </div>
+            <div className="md p-4 ">
+              <h3 className="text-sm font-semibold text-gray-500 mb-2">Biljettsammanfattning</h3>  
+            </div>
           </div>
-          <div className="md p-4 mt-4">
-            <h3 className="text-sm font-semibold text-gray-500 mb-2">Biljettsammanfattning</h3>  
+          <div className="bg-zinc-800 rounded-lg w-64 shadow-lg border border-zinc-700 flex flex-col justify-center items-center">
+           <h3 className="text-sm font-semibold text-gray-500 mb-2">Bokningsnummer</h3>
+              <div className="text-2xl font-bold text-white mb-2">BC-HUNQAGOVKYRQ</div>
+            
           </div>
         </div>
       </div>
