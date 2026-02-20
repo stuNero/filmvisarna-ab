2de27801494fe931f3249593a31d8fb937bd125f
Piercor <pierinocr@gmail.com>
2026-02-20 14:57:45 +0100

    add/ fetch to get actors data

--- src/pages/MovieDetailShowingsPage.tsx
+++ src/pages/MovieDetailShowingsPage.tsx
@@ -15,6 +15,8 @@ export default function MovieDetailShowingsPage() {
   const movieId = Number(id);
 
   const [details] = useFetchJson<MovieDetails | null>(`/api/comingFilms/${movieId}`);
+  const [actors] = useFetchJson<{ name: string; }[]>(`/api/movieActors?WHERE=id=${movieId}`);
+
 
   if (details?.id === movieId) {
     return <>
@@ -27,6 +29,11 @@ export default function MovieDetailShowingsPage() {
               <h3 className="text-xl md:text-3xl font-extrabold">{details?.productionYear}</h3>
               <h3 className="text-xl md:text-3xl font-extrabold">{details?.length} minuter</h3>
               <h3 className="text-lg md:text-xl font-extrabold">{details?.genre} </h3>
+            </div>
+            <div className="flex flex-row gap-5 h-12.5 text-base md:text-xl font-medium">
+              {actors?.map((a, index) => (
+                <h5 key={index}>{a.name}</h5>
+              ))}
             </div>
             <div>
               <p className="text-base md:text-xl font-bold">{details?.filmDescription}</p>
