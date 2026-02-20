9795d964c58cac2ca27664a873b19e80602872bb
jacoblasse <jacoblasse4@gmail.com>
2026-02-20 15:01:52 +0100

    changed min-h

--- src/pages/BookingConfirmationPage.tsx
+++ src/pages/BookingConfirmationPage.tsx
@@ -6,7 +6,7 @@ BookingConfirmationPage.route = {
 
 export default function BookingConfirmationPage() {
   return (
-    <div className="min-h-[75vh] flex items-center justify-center">
+    <div className="min-h-screen flex items-center justify-center">
       <div className="bg-zinc-950 rounded-lg max-w-3xl w-full shadow-lg border border-zinc-700 overflow-hidden flex flex-col">
         <div className="bg-red-800 p-6 text-center">
           <h2 className="text-2xl font-bold text-white mb-2">Bokning bekräftad!</h2>
