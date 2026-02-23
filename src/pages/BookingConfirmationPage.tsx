d92be9339a4e9c43fcd4d7ad19ce76550e1543c7
jacoblasse <jacoblasse4@gmail.com>
2026-02-23 13:46:22 +0100

    Made change so that the page is more responsive and works on different screen sizes

--- src/pages/BookingConfirmationPage.tsx
+++ src/pages/BookingConfirmationPage.tsx
@@ -23,18 +23,18 @@ const mockData = {
 
 export default function BookingConfirmationPage() {
   return (
-    <div className="min-h-screen flex items-center justify-center">
+    <div className="min-h-screen flex items-center justify-center px-2 sm:px-4">
       <div className="bg-zinc-950 rounded-lg max-w-4xl mx-auto w-full shadow-lg border border-zinc-700 overflow-hidden flex flex-col">
-        <div className="bg-red-800 p-6 text-center">
+        <div className="bg-red-800 p-4 sm:p-6 text-center">
           <div className="inline-flex items-center justify-center w-20 h-20 bg-white/20 rounded-full mb-4">
               <CheckCircle2 className="w-10 h-10 text-white" />
             </div>
           <h2 className="text-2xl font-bold text-white mb-2">Bokning bekräftad!</h2>
           <p className="text-white/90">Tack för att du väljer vår bio. Dina biljetter är klara.</p>
         </div>
-        <div className="p-8 flex flex-row gap-6">
+        <div className="p-4 sm:p-8 flex flex-col md:flex-row gap-4 md:gap-6">
           <div className="flex-1 flex flex-col gap-4">
-            <div className=" p-4 flex flex-col gap-4">
+            <div className="p-2 sm:p-4 flex flex-col gap-4">
               <div className="md">
                 <h3 className="text-sm font-semibold text-gray-500 mb-2">Filmdetaljer</h3>
                 <h2 className="text-2xl font-bold text-white mb-2">{mockData.movie.title}</h2>
@@ -56,7 +56,7 @@ export default function BookingConfirmationPage() {
               </div>
             </div>
           </div>
-          <div className="bg-zinc-800 rounded-lg w-64 shadow-lg border border-zinc-700 flex flex-col justify-start items-center p-4">
+          <div className="bg-zinc-800 rounded-lg w-full md:w-64 shadow-lg border border-zinc-700 flex flex-col justify-start items-center p-4 mt-4 md:mt-0">
             <div className="mb-4 w-full text-center">
               <h3 className="text-sm font-semibold text-gray-500 mb-2">Bokningsnummer</h3>
               <h2 className="text-2xl font-bold text-white mb-2">BC-HUNQAGOVKYRQ</h2>
