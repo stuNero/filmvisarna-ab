46bcfcaa53c08cd08df1f6117ef05a667add8333
Jacob Larsson <jacoblasse4@gmail.com>
2026-02-24 09:29:16 +0100

    Fixed the back button to the left of the screen

--- src/pages/BookingConfirmationPage.tsx
+++ src/pages/BookingConfirmationPage.tsx
@@ -27,12 +27,12 @@ const mockData = {
 
 export default function BookingConfirmationPage() {
   return (
-    <div className="min-h-screen flex items-center justify-center px-2 sm:px-4 flex flex-col pt-18">
+    <div className="min-h-screen mx-auto max-w-4xl px-2 sm:px-4 flex flex-col pt-18">
       <button className="inline-flex  gap-2 text-gray-400 hover:text-white mb-8 transition-colors group">
         <ChevronLeft className="w-5 h-5 group-hover:-translate-x-1 transition-transform" />
         Tillbaka till filmerna
       </button>
-      <div className="bg-zinc-950 rounded-3xl max-w-4xl mx-auto w-full shadow-lg border border-zinc-700 overflow-hidden ">
+      <div className="bg-zinc-950 rounded-3xl  mx-auto w-full shadow-lg border border-zinc-700 overflow-hidden ">
         <div className="bg-red-800 p-4 sm:p-6 text-center">
           <div className="inline-flex items-center justify-center w-20 h-20 bg-white/20 rounded-full mb-4">
             <CheckCircle2 className="w-10 h-10 text-white" />
