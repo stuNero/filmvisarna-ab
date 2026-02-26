2cff3c02e22b79ef7ec167457ad058721d73f352
amir247 <amir_hamza@outlook.com>
2026-02-26 11:31:18 +0100

    add specialRoutes.start in server.cs file

--- backend/src/Server.cs
+++ backend/src/Server.cs
@@ -11,6 +11,7 @@ public static class Server
         ErrorHandler.Start();
         FileServer.Start();
         LoginRoutes.Start();
+        SpecialRoutes.Start();
         RestApi.Start();
         Session.Start();
         // Start the server on port 3001
