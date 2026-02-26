37fd20720df84e3e45bf15e29a681fc27f204e87
amir247 <amir_hamza@outlook.com>
2026-02-26 13:44:30 +0100

    fixed header related visibility issue

--- src/partials/Main.tsx
+++ src/partials/Main.tsx
@@ -1,12 +1,16 @@
-import { Outlet } from 'react-router-dom';
+import { Outlet } from "react-router-dom";
 
 export default function Main() {
   return (
-    <main className="min-h-screen lg:px-8 
+    <main
+      className="min-h-screen lg:px-8 
                     container 
                     mx-auto 
+                    my-auto
                     justify-center
-                    ">
+                    pt-20
+                    "
+    >
       <Outlet />
     </main>
   );
