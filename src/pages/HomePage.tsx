a66ebc579f349b05a4a8ecad4a39a651de48ca22
amir247 <amir_hamza@outlook.com>
2026-03-04 18:08:54 +0100

    fixed hero text styling

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -8,18 +8,14 @@ HomePage.route = {
 
 export default function HomePage() {
   return (
-    <div
-      className="hero flex flex-col 
-                        "
-    >
+    <div className="flex flex-col  ">
       <section className="flex inline-block relative">
         <section
           className="
                         bg-[url('/bg-image.webp')] bg-center
                         bg-cover                       
                         h-[75vh]
-                        z-0
-                        "
+                        z-0"
         >
           <div
             className="absolute inset-0 
@@ -28,7 +24,7 @@ export default function HomePage() {
                         from-black from-20% via-black/50 via-50% to-transparent to-90%"
           ></div>
 
-          <div className="absolute mt-10 z-20 text-shadow-lg">
+          <div className="absolute container px-4 mx-auto mt-10 z-20 text-shadow-lg">
             <h1 className="text-6xl">Upplev bio som aldrig förr</h1>
             <p className="text-2xl mt-4">
               Fördjupa dig i de senaste storfilmerna med toppmodern ljudteknik
@@ -38,7 +34,7 @@ export default function HomePage() {
         </section>
       </section>
 
-      <section>
+      <section className="mx-auto">
         <div className="mt-5">
           <h2 className="text-center text-6xl pt-2 pb-2">Visas nu</h2>
         </div>
