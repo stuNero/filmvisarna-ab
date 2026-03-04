0278d715aab4111490b5b282ae230f82cb8890e2
Piercor <pierinocr@gmail.com>
2026-03-04 16:23:43 +0100

    add/ styling to increase/decrease buttons

--- src/parts/SeatType.tsx
+++ src/parts/SeatType.tsx
@@ -1,3 +1,4 @@
+
 interface SeatTypeProps {
     name: string;
     info: string;
@@ -7,7 +8,7 @@ interface SeatTypeProps {
     decrementTicketCount: () => void;
 }
 
-export default function SeatType({ name, info, price, ticketCount, incrementTicketCount , decrementTicketCount}: SeatTypeProps) {
+export default function SeatType({ name, info, price, ticketCount, incrementTicketCount, decrementTicketCount }: SeatTypeProps) {
     return <>
         <div className="flex items-center justify-between bg-black rounded-lg p-4 border border-white/10">
             <div>
@@ -16,9 +17,13 @@ export default function SeatType({ name, info, price, ticketCount, incrementTick
                 <p className="text-red-500 font-medium">{price}kr</p>
             </div>
             <div className="flex items-center gap-3">
-                <button className="px-4 py-2 text-white outline-solid rounded hover:bg-red-600" onClick={decrementTicketCount}>-</button>
+                <button className={ticketCount > 0 ? `px-4 py-2 font-extrabold size-10 text-stone-500 outline-solid rounded hover:bg-red-600`
+                    : `px-4 py-2 text-stone-800 outline-solid rounded`
+                } onClick={decrementTicketCount}>-</button>
                 <p>{ticketCount}</p>
-                <button className="px-4 py-2 text-white outline-solid rounded hover:bg-red-600" onClick={incrementTicketCount}>+</button>
+                <button className={ticketCount < 8 ? `px-4 py-2 font-extrabold size-10 text-stone-500 outline-solid rounded hover:bg-red-600`
+                    : `px-4 py-2 text-stone-800 outline-solid rounded`
+                } onClick={incrementTicketCount}>+</button>
             </div>
         </div>
     </>;
