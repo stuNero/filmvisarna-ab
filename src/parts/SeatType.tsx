87021ee40da6d248f035b0d01d1f6919cac1d269
Piercor <pierinocr@gmail.com>
2026-03-04 16:38:12 +0100

    add/ ternary check when clicking the +/- buttons

--- src/parts/SeatType.tsx
+++ src/parts/SeatType.tsx
@@ -7,8 +7,8 @@ interface SeatTypeProps {
     incrementTicketCount: () => void;
     decrementTicketCount: () => void;
 }
-
 export default function SeatType({ name, info, price, ticketCount, incrementTicketCount, decrementTicketCount }: SeatTypeProps) {
+
     return <>
         <div className="flex items-center justify-between bg-black rounded-lg p-4 border border-white/10">
             <div>
@@ -19,11 +19,11 @@ export default function SeatType({ name, info, price, ticketCount, incrementTick
             <div className="flex items-center gap-3">
                 <button className={ticketCount > 0 ? `px-4 py-2 font-extrabold size-10 text-stone-500 outline-solid rounded hover:bg-red-600`
                     : `px-4 py-2 text-stone-800 outline-solid rounded`
-                } onClick={decrementTicketCount}>-</button>
+                } onClick={ticketCount > 0 ? decrementTicketCount : () => { }}>-</button>
                 <p>{ticketCount}</p>
                 <button className={ticketCount < 8 ? `px-4 py-2 font-extrabold size-10 text-stone-500 outline-solid rounded hover:bg-red-600`
                     : `px-4 py-2 text-stone-800 outline-solid rounded`
-                } onClick={incrementTicketCount}>+</button>
+                } onClick={ticketCount < 8 ? incrementTicketCount : () => { }}>+</button>
             </div>
         </div>
     </>;
