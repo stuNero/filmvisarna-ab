d382603230623dc95eda8e0aa668b5f4e9f995f9
amir247 <amir_hamza@outlook.com>
2026-03-04 17:48:52 +0100

    add background gradient for better text visibility

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,32 +1,49 @@
 import MovieCard from "../parts/MovieCard.tsx";
 
 HomePage.route = {
-  path: '/',
-  menuLabel: 'Home',
-  index: 1
+  path: "/",
+  menuLabel: "Home",
+  index: 1,
 };
 
 export default function HomePage() {
-  return <div className="hero flex flex-col 
-                        ">
-    <section className="
+  return (
+    <div
+      className="hero flex flex-col 
+                        "
+    >
+      <section className="flex inline-block relative">
+        <section
+          className="
                         bg-[url('/bg-image.webp')] bg-center
-                        bg-no-repeat                        
-                        top-0 bottom-0 left-0 h-[75vh]
-                        content-center justify-center
-                        ">
-      <div className="scale-90 mb-50 md:w-[50vw] md:ml-50 md:scale-125">
-        <h1 className="text-3xl">Upplev bio som aldrig förr</h1>
-        <p>
-          Fördjupa dig i de senaste storfilmerna med toppmodern ljudteknik och <br /> fantastisk bild. Boka dina biljetter nu.
-        </p>
-      </div>
-    </section>
-    <section>
-      <div className="mt-5">
-        <h2 className="text-center text-6xl pt-2 pb-2">Visas nu</h2>
-      </div>
-      <MovieCard />
-    </section>
-  </div>;
+                        bg-cover                       
+                        h-[75vh]
+                        z-0
+                        "
+        >
+          <div
+            className="absolute inset-0 
+                          z-10
+                          bg-gradient-to-t
+                        from-black from-20% via-black/50 via-50% to-transparent to-90%"
+          ></div>
+
+          <div className="absolute mt-10 z-20 text-shadow-lg">
+            <h1 className="text-6xl">Upplev bio som aldrig förr</h1>
+            <p className="text-2xl mt-4">
+              Fördjupa dig i de senaste storfilmerna med toppmodern ljudteknik
+              och <br /> fantastisk bild. Boka dina biljetter nu.
+            </p>
+          </div>
+        </section>
+      </section>
+
+      <section>
+        <div className="mt-5">
+          <h2 className="text-center text-6xl pt-2 pb-2">Visas nu</h2>
+        </div>
+        <MovieCard />
+      </section>
+    </div>
+  );
 }
