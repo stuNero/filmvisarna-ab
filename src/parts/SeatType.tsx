7eb52c17007a35e822ec014da6cf102f2136d9cc
Piercor <pierinocr@gmail.com>
2026-03-04 23:49:59 +0100

    add/ function to disable +/- buttons
    when the total amount of selected seats is greater than 8 or less than
    1, the +/- buttons would be disabled respectively.

--- src/parts/SeatType.tsx
+++ src/parts/SeatType.tsx
@@ -7,7 +7,23 @@ interface SeatTypeProps {
     incrementTicketCount: () => void;
     decrementTicketCount: () => void;
 }
+
+let totalTickets = 0;
+
 export default function SeatType({ name, info, price, ticketCount, incrementTicketCount, decrementTicketCount }: SeatTypeProps) {
+
+    function incrementTicket() {
+        if (ticketCount < 8) {
+            incrementTicketCount();
+        }
+        if (totalTickets < 8) { totalTickets++; }
+    }
+    function decrementTicket() {
+        if (ticketCount > 0) {
+            decrementTicketCount();
+        }
+        if (totalTickets > 0) { totalTickets--; }
+    }
 
     return <>
         <div className="flex items-center justify-between bg-black rounded-lg p-4 border border-white/10">
@@ -17,13 +33,13 @@ export default function SeatType({ name, info, price, ticketCount, incrementTick
                 <p className="text-red-500 font-medium">{price}kr</p>
             </div>
             <div className="flex items-center gap-3">
-                <button className={ticketCount > 0 ? `px-4 py-2 font-extrabold size-10 text-stone-500 outline-solid rounded hover:bg-red-600`
+                <button className={totalTickets > 0 && ticketCount > 0 ? `px-4 py-2 font-extrabold size-10 text-stone-500 outline-solid rounded hover:bg-red-600`
                     : `px-4 py-2 text-stone-800 outline-solid rounded`
-                } onClick={decrementTicketCount}>-</button>
+                } onClick={decrementTicket}>-</button>
                 <p>{ticketCount}</p>
-                <button className={ticketCount < 8 ? `px-4 py-2 font-extrabold size-10 text-stone-500 outline-solid rounded hover:bg-red-600`
+                <button className={totalTickets < 8 ? `px-4 py-2 font-extrabold size-10 text-stone-500 outline-solid rounded hover:bg-red-600`
                     : `px-4 py-2 text-stone-800 outline-solid rounded`
-                } onClick={incrementTicketCount}>+</button>
+                } onClick={incrementTicket}>+</button>
             </div>
         </div>
     </>;
