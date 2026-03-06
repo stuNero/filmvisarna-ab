dee611fe2c15426ba5b64772888231e1233966f0
stuNero <max.vemic@student.nbi-handelsakademin.se>
2026-03-06 11:24:54 +0100

    add id to bookingInfo

--- backend/src/DbQuery.cs
+++ backend/src/DbQuery.cs
@@ -184,18 +184,19 @@ public static class DbQuery
                     s.rowNr,
                     s.columnNr,
                     sh.timeSlot,
-                    v.name AS venueName,
-                    v.info AS venueInfo,
-                    f.title AS filmTitle,
                     bs.ticketType,
-                    b.id AS bookingId,
-                    b.cost AS totalPrice
+                    v.name  AS venueName,
+                    v.info  AS venueInfo,
+                    f.id    AS filmID,
+                    f.title AS filmTitle,
+                    b.id    AS bookingId,
+                    b.cost  AS totalPrice
                 FROM bookedSeat bs
-                JOIN bookings b ON bs.bookingId = b.id
-                JOIN seats s ON bs.seatId = s.id
-                JOIN showings sh ON b.showingId = sh.id
-                JOIN venues v ON sh.venueId = v.id
-                JOIN films f ON sh.filmId = f.id
+                JOIN bookings   b   ON bs.bookingId = b.id
+                JOIN seats      s   ON bs.seatId    = s.id
+                JOIN showings   sh  ON b.showingId  = sh.id
+                JOIN venues     v   ON sh.venueId   = v.id
+                JOIN films      f   ON sh.filmId    = f.id
             ;
             DROP VIEW IF EXISTS showingsAllSeats;
             CREATE VIEW showingsAllSeats AS
--- src/interfaces/BookingInfo.ts
+++ src/interfaces/BookingInfo.ts
@@ -4,6 +4,7 @@ export default interface bookingInfo {
   timeSlot: string;
   venueName: string;
   venueInfo: string;
+  filmID: string;
   filmTitle: string;
   ticketType: string;
   bookingId: string;
