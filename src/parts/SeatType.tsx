95f00d85327955503ecedfef2d79f7fa9c05f484
Piercor <pierinocr@gmail.com>
2026-03-07 21:35:43 +0100

    fix/ +/- symbols centered in the button
    also change the text to emojis for better alignnment

--- src/parts/SeatType.tsx
+++ src/parts/SeatType.tsx
@@ -33,13 +33,13 @@ export default function SeatType({ name, info, price, ticketCount, incrementTick
         <p className="text-red-500 font-medium">{price}kr</p>
       </div>
       <div className="flex items-center gap-3">
-        <button className={totalTickets > 0 && ticketCount > 0 ? `px-4 py-2 font-extrabold size-10 text-stone-500 outline-solid rounded hover:bg-red-600`
-          : `px-4 py-2 font-extrabold size-10 text-stone-800 outline-solid rounded`
-        } onClick={decrementTicket}>-</button>
+        <button className={totalTickets > 0 && ticketCount > 0 ? `flex justify-center items-center px-4 py-2 text-[10px] size-10 text-stone-500 outline-solid rounded hover:bg-red-600`
+          : `flex justify-center items-center px-4 py-2 text-[10px] size-10 text-stone-800 outline-solid rounded`
+        } onClick={decrementTicket}>➖</button>
         <p>{ticketCount}</p>
-        <button className={totalTickets < 8 ? `px-4 py-2 font-extrabold size-10 text-stone-500 outline-solid rounded hover:bg-red-600`
-          : `px-4 py-2 font-extrabold size-10 text-stone-800 outline-solid rounded`
-        } onClick={incrementTicket}>+</button>
+        <button className={totalTickets < 8 ? `flex justify-center items-center px-4 py-2 text-[10px] size-10 text-stone-500 outline-solid rounded hover:bg-red-600`
+          : `flex justify-center items-center px-4 py-2 text-[10px] size-10 text-stone-800 outline-solid rounded`
+        } onClick={incrementTicket}>➕</button>
       </div>
     </div>
   </>;
