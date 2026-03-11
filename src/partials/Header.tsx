cecb80a855ce56d3162a184fc527ced167acfe3d
amir247 <amir_hamza@outlook.com>
2026-03-11 22:39:30 +0100

    add logut button ui and functionality in header

--- src/partials/Header.tsx
+++ src/partials/Header.tsx
@@ -1,9 +1,14 @@
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import { User } from 'lucide-react';
 import LoginPage from '../pages/LoginPage';
 import HomePage from '../pages/HomePage';
+import { useAuth } from '../pages/AuthProvider';
+import fetchJson from '../utils/fetchJson';
+import ProfilePage from '../pages/ProfilePage';
 
 export default function Header() {
+  const navigate = useNavigate();
+  const { user, setUser } = useAuth();
   return (
     <header
       className="w-full 
@@ -23,9 +28,41 @@ export default function Header() {
           <img src="/logo-cinesharp.webp" width="40px" height="auto"></img>
           <h1 className="font-bold content-center pl-2.5 ">CineSharp</h1>
         </Link>
-        <Link to={LoginPage.route.path} className="hover-red my-2.5 w-7.5 h-auto">
-          <User className="my-2.5 w-7.5 h-auto" />
-        </Link>
+
+        {/* user button behaviour */}
+        <div className="flex space-x-6">
+          {/* routing logic for the logedin vs logedout user */}
+          {user ? (
+            <Link
+              to={ProfilePage.route.path}
+              className="hover-red my-2.5 w-7.5 h-auto"
+            >
+              <User className="my-2.5 w-7.5 h-auto" />
+            </Link>
+          ) : (
+            <Link
+              to={LoginPage.route.path}
+              className="hover-red my-2.5 w-7.5 h-auto"
+            >
+              <User className="my-2.5 w-7.5 h-auto" />
+            </Link>
+          )}
+
+          {/* routing logic when logout button is pressed */}
+          {user && (
+            <button
+              className="cursor-pointer"
+              onClick={async () => {
+                await fetchJson('/api/login', { method: 'DELETE' });
+
+                setUser(null);
+                window.location.href = '/logga-in';
+              }}
+            >
+              Logga ut
+            </button>
+          )}
+        </div>
       </div>
     </header>
   );
