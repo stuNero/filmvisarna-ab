bddf27756ec829bd1980b195a944698c37e4a96f
OscarWEdu <oscar.ward@student.nbi-handelsakademin.se>
2026-03-11 14:11:08 +0100

    Turn Confirmation of booking cancellation into its own component, so we can reuse it

--- src/pages/CancelBookingPage.tsx
+++ src/pages/CancelBookingPage.tsx
@@ -1,11 +1,11 @@
-import { Calendar, Ticket, MapPin, Clock, Search, CheckCircle2, Mail } from 'lucide-react';
+import { Calendar, Ticket, MapPin, Clock, Search, Mail } from 'lucide-react';
 import { useState, useEffect, useRef } from "react";
 import useFetchJson from "../utils/useFetchJson";
 import fetchJson from '../utils/fetchJson';
 import type bookingInfo from "../interfaces/BookingInfo";
-import { useNavigate } from 'react-router-dom';
 import type MovieDetails from '../interfaces/MovieDetails';
 import { useSearchParams } from 'react-router-dom';
+import UnbookConfirmed from '../parts/UnbookConfirmed';
 
 CancelBookingPage.route = {
   path: '/avboka',
@@ -14,7 +14,6 @@ CancelBookingPage.route = {
 
 export default function CancelBookingPage() {
 
-  const navigate = useNavigate();
   const [searchParams] = useSearchParams();
 
   // Initializing page variables
@@ -304,34 +303,7 @@ export default function CancelBookingPage() {
         </section>
       ) : (
         // Section when cancellation is confirmed
-        <section className="
-        mt-30 min-h-screen
-        w-1/2
-        px-2 sm:px-4
-        flex flex-col
-        pt-18 pb-8">
-          <div className="bg-zinc-950 rounded-3xl  mx-auto w-full shadow-lg border border-zinc-700 overflow-hidden">
-            <div className="bg-green-800 p-4 sm:p-6 text-center">
-              <div className="inline-flex items-center justify-center w-20 h-20 bg-white/20 rounded-full mb-4">
-                <CheckCircle2 className="w-10 h-10 text-white" />
-              </div>
-              <h2 className="text-2xl font-bold text-white mb-2">
-                Avbokning bekräftad!
-              </h2>
-              <p className="text-white/90">
-                Tack för att du väljer vår bio.
-              </p>
-            </div>
-
-            <div className="bg-zinc-900/50 p-6 sm:p-6 text-center">
-              <button
-                className='rounded-2xl bg-gray-800 px-7 py-2 hover:bg-gray-900 scale-130'
-                onClick={() => navigate("/")}>
-                Gå till hemsidan
-              </button>
-            </div>
-          </div>
-        </section>
+        <UnbookConfirmed/>
       )}
     </div>
   );
