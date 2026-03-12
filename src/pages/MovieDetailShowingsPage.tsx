25d7ea3cc634dba2ed017bd5b135b893f310b8b7
jacoblasse <jacoblasse4@gmail.com>
2026-03-12 11:32:48 +0100

    changed max-width to 7xl  in MovieDetailShowingsPage for better look

--- src/pages/MovieDetailShowingsPage.tsx
+++ src/pages/MovieDetailShowingsPage.tsx
@@ -60,7 +60,7 @@ export default function MovieDetailShowingsPage() {
   if (details?.id === movieId) {
     return (
       <>
-        <section>
+        <section className="max-w-7xl mx-auto pt-10 px-4">
           <div className="min-h-screen flex flex-col justify-center px-4 bg-[url('/cinema.webp')] bg-no-repeat bg-center ">
             <div className="flex flex-col md:flex-row md:items-start">
               <img
@@ -124,7 +124,7 @@ export default function MovieDetailShowingsPage() {
                     ))}
                   </div>
                 </div>
-                <div className="mb-6 md:mb-0">
+                <div className="mb-6 md:mb-4">
                   <h3 className="text-sm text-gray-400 mb-1">Sammanfattning</h3>
                   <p className="text-base md:text-xl font-bold">
                     {details?.filmDescription}
@@ -138,7 +138,7 @@ export default function MovieDetailShowingsPage() {
         <section
           className="flex flex-col 
               bg-stone-950
-              pt-2.5 pb-2.5 rounded-lg mb-16 px-2 sm:px-4 mx-2"
+              pt-2.5 pb-2.5 rounded-lg mb-16 px-2 sm:px-4 mx-2 max-w-7xl mx-auto"
         >
           <div className="flex justify-center pt-5 pb-5">
             <h2 className="text-2xl font-bold">Välj en visning</h2>
