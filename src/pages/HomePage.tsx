2e04033865fbe7d77c370f109d60aca81fe58a1b
stuNero <max.vemic@student.nbi-handelsakademin.se>
2026-03-12 15:42:04 +0100

    add kiosk card to homepage

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,4 +1,6 @@
+import { Link } from 'react-router-dom';
 import MovieCard from '../parts/MovieCard.tsx';
+import KioskInfoPage from './KioskPage.tsx';
 
 HomePage.route = {
   path: '/'
@@ -9,29 +11,27 @@ export default function HomePage() {
     <div className="container mx-auto">
       <div className="flex flex-col  ">
         <section className="inline-block relative">
-          <section
-            className="
-                        bg-[url('/bg-image.webp')] bg-center
-                        bg-cover                       
-                        h-[75vh]
-                        fit-content
-                        "
-          >
-            <div
-              className="absolute inset-0 
-                          bg-linear-to-t
-                        from-black from-20% via-black/50 via-70% to-transparent to-99%
-                        fit-content"
-            ></div>
-
-            <div
-              className="absolute container 
+          <section className="
+            bg-[url('/bg-image.webp')] bg-center
+            bg-cover                       
+            h-[75vh]
+            fit-content
+            ">
+            <div className="
+              absolute inset-0 
+              bg-linear-to-t
+              from-black from-20% via-black/50 via-70% to-transparent to-99%
+              fit-content"/>
+            <div className="
+              md:flex md:flex-col
+              md:items-center
+              md:gap-50
+              absolute container 
               px-4 mx-auto mt-10 
               text-shadow-lg
               pt-20
-                          "
-            >
-              <div className="max-w-7xl mx-auto px-4 sm:px-6 md:px-8 lg:px-16 min-w-xs ">
+                          ">
+              <div className="md:-translate-x-50 max-w-7xl mx-auto px-4 sm:px-6 md:px-8 lg:px-16 min-w-xs md:relative">
                 <h1 className="text-5xl md:text-7xl font-normal md:max-w-xl">
                   Upplev bio som aldrig förr
                 </h1>
@@ -40,6 +40,37 @@ export default function HomePage() {
                   ljudteknik och <br /> fantastisk bild. Boka dina biljetter nu.
                 </p>
               </div>
+              <Link
+                to={KioskInfoPage.route.path}
+                className="
+                md:content-center
+                md:text-3xl
+                md:scale-150
+                md:translate-x-90
+                md:-translate-y-90
+                md:overflow-hidden
+                md:hover:scale-155
+                text-shadow-black
+                text-shadow-md
+                block
+                w-full md:w-70 md:h-100
+                -translate-y-1
+                backdrop-blur
+                border-t border-l border-zinc-700
+                rounded-xl
+                px-6 py-4
+                text-center
+                text-lg
+                bg-[url('/snack-bar.webp')]
+                bg-cover
+                bg-bottom
+                transition
+                "
+              >
+                <h1>
+                  Utforska vårat kiosk utbud!
+                </h1>
+              </Link>
             </div>
           </section>
         </section>
