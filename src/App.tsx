f8c5965d2a3468efb1c31dd3fa4821be1a621aa9
amir247 <amir_hamza@outlook.com>
2026-03-13 01:53:38 +0100

    add function to restore user if loged out accidently by page reloade

--- src/App.tsx
+++ src/App.tsx
@@ -2,20 +2,37 @@ import { useLocation } from 'react-router-dom';
 import Header from './partials/Header';
 import Main from './partials/Main';
 import Footer from './partials/Footer';
-import { AuthProvider } from './pages/AuthProvider';
+import { useAuth } from './pages/AuthProvider';
+import fetchJson from './utils/fetchJson';
+import { useEffect } from 'react';
 
 export default function App() {
   // scroll to top when the route changes
   useLocation();
   window.scrollTo({ top: 0, left: 0, behavior: 'instant' });
 
+  const { setUser } = useAuth();
+  // if a user gets loged out in frontend by page relode, not by clicking on logout button then this useeffect restorse the actual state of the user, which is logedin.
+  useEffect(() => {
+    const checkSession = async () => {
+      try {
+        const result = await fetchJson('/api/login', { method: 'GET' });
+        if (result && !result.error) {
+          setUser(result); // restore user from session!
+        }
+      } catch (error) {
+        console.error('Session check failed:', error);
+      }
+    };
+
+    checkSession();
+  }, []); // run once when the app loads
+
   return (
     <div className="max-w-full overflow-x-hidden">
-      <AuthProvider>
-        <Header />
-        <Main />
-        <Footer />
-      </AuthProvider>
+      <Header />
+      <Main />
+      <Footer />
     </div>
   );
 }
