bc47f0961f4c182db3ec178189a14e45d76bb4bf
Jacob Larsson <jacoblasse4@gmail.com>
2026-03-13 15:18:41 +0100

    unfinished version ShowingPage

--- src/pages/ShowingsPage.tsx
+++ src/pages/ShowingsPage.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { useParams, Link } from "react-router-dom";
 import useFetchJson from "../utils/useFetchJson";
 import NotFoundPage from "./NotFoundPage";
@@ -13,10 +13,19 @@ ShowingsPage.route = {
 
 export default function ShowingsPage() {
   const { id } = useParams<{ id: string }>();
-  const [showTrailer, setShowTrailer] = useState(false);
   const movieId = Number(id);
-  const [switchSections, setSwitchSections] = useState(false);
 
+  const [showTrailer, setShowTrailer] = useState(false);
+  const [switchSections, setSwitchSections] = useState(false);
+  const [viewing, setViewing] = useState("");
+  console.log(window.innerWidth);
+  useEffect(() => {
+    if (window.innerWidth < 400) {
+      setViewing("mobile");
+    } else {
+      setViewing("desktop");
+    }
+  });
   const [details] = useFetchJson<MovieDetails | null>(
     `/api/comingFilms/${movieId}`,
   );
@@ -81,51 +90,254 @@ export default function ShowingsPage() {
   }
 
   if (details?.id === movieId) {
-    return (
-      <>
-        <div className="mt-20 flex flex-row">
-          <Link
-            className="flex row items-center w-170"
-            to={HomePage.route.path}
-          >
-            <MoveLeft className="mx-5" />
-            Back to Home Page
-          </Link>
-          <h1 className="flex items-center bg-stone-800  p-5 ml-10 scale-120 w-full rounded-l-2xl h-10">
-            {details.title}
-          </h1>
-        </div>
-        <div className="md:hidden flex flex-row mt-10 mb-0 w-screen">
-          <button
-            onClick={() => setSwitchSections(true)}
-            className={
-              switchSections
-                ? `rounded-tl-2xl border border-solid border-stone-600
-            h-10 w-[50%] mb-0
-            bg-gray-800 border-b-0`
-                : `rounded-tl-2xl border border-solid border-stone-600
-          h-10 w-[50%] mb-0
-          bg-black`
-            }
-          >
-            Detaljer
-          </button>
-          <button
-            onClick={() => setSwitchSections(false)}
-            className={
-              switchSections
-                ? `rounded-tr-2xl border border-solid border-stone-600
+    if (viewing == "mobile") {
+      return (
+        <>
+          <div className="mt-20 flex flex-row">
+            <Link
+              className="flex row items-center w-170"
+              to={HomePage.route.path}
+            >
+              <MoveLeft className="mx-5" />
+              Back to Home Page
+            </Link>
+            <h1 className="flex items-center bg-stone-800  p-5 ml-10 scale-120 w-full rounded-l-2xl h-10">
+              {details.title}
+            </h1>
+          </div>
+          <div className="md:hidden flex flex-row mt-10 mb-0 w-screen">
+            <button
+              onClick={() => setSwitchSections(false)}
+              className={
+                switchSections
+                  ? `rounded-tl-2xl border border-solid border-stone-600
+                h-10 w-[50%] mb-0
+                bg-gray-800 border-b-0`
+                  : `rounded-tl-2xl border border-solid border-stone-600
                 h-10 w-[50%] mb-0
                 bg-black`
-                : `rounded-tr-2xl border border-solid border-stone-600
+              }
+            >
+              Detaljer
+            </button>
+            <button
+              onClick={() => setSwitchSections(false)}
+              className={
+                switchSections
+                  ? `rounded-tr-2xl border border-solid border-stone-600
+                h-10 w-[50%] mb-0
+                bg-black`
+                  : `rounded-tr-2xl border border-solid border-stone-600
             h-10 w-[50%] mb-0
             bg-gray-800 border-b-0`
-            }
-          >
-            Visningar
-          </button>
-        </div>
-        {switchSections ? (
+              }
+            >
+              Visningar
+            </button>
+          </div>
+          {switchSections ? (
+            <section className={switchSections ? "md:hi" : ""}>
+              <div className="md:mt-10 flex flex-col justify-center px-4 bg-[url('/cinema.webp')] bg-no-repeat bg-center ">
+                <div
+                  className={`flex flex-col md:flex-row md:items-start max-w-7xl mx-auto px-8 sm:px-12 lg:px-16`}
+                >
+                  <img
+                    src={details?.coverImage}
+                    alt={details?.title}
+                    className="w-full max-w-sm md:max-w-sm aspect-2/3 h-auto md:max-h-137.5 p-5 rounded-3xl object-contain md:object-cover"
+                  />
+                  <div className="md:ml-8 ">
+                    <div className="flex flex-col lg:gap-10 md:gap-2 pb-5 pt-5 mb-6 w-full">
+                      <div className="w-full block">
+                        <h1 className="text-3xl md:text-5xl font-extrabold w-full mb-4">
+                          {details?.title}
+                        </h1>
+                      </div>
+                      <div className="w-full block">
+                        <div className="flex flex-row flex-wrap gap-4 w-full mb-2">
+                          <span className="px-3 py-1 border border-gray-600 rounded text-white">
+                            {details?.ageRating} år
+                          </span>
+                          <h3 className="text-xl md:text-lg py-1">
+                            {details?.productionYear}
+                          </h3>
+                          <div className="flex items-center gap-2">
+                            <Clock size={16} className="-mb-0.5" />
+                            <h3 className="text-lg md:text-lg ">
+                              {details?.length} min
+                            </h3>
+                          </div>
+
+                          <span className="px-3 py-1 bg-white/5 rounded">
+                            {details?.genre}
+                          </span>
+                          <button
+                            className="flex items-center gap-2 px-4 py-1.5 bg-red-800 text-white rounded transition-colors"
+                            onClick={() => setShowTrailer(true)}
+                          >
+                            <p className="font-medium">Se trailer</p>
+                          </button>
+                        </div>
+                      </div>
+                    </div>
+                    <div className="mb-6  text-base md:text-xl font-medium">
+                      <h3 className="text-sm text-gray-400 mb-1">Regissör</h3>
+                      <h5>{details.director}</h5>
+                    </div>
+                    <div className="mb-6 md:mb-0">
+                      <h3 className="text-sm text-gray-400 mb-1">
+                        Skådespelare
+                      </h3>
+                      <div className="flex flex-row gap-5 h-12.5 text-base md:text-xl font-medium">
+                        {actors?.map((a, index) => (
+                          <h5 key={index}>{a.name}</h5>
+                        ))}
+                      </div>
+                    </div>
+                    <div className="mb-6 md:mb-4">
+                      <h3 className="text-sm text-gray-400 mb-1">
+                        Sammanfattning
+                      </h3>
+                      <p className="text-base md:text-xl font-bold">
+                        {details?.filmDescription}
+                      </p>
+                    </div>
+                  </div>
+                </div>
+
+                {/* R E V I E W   S E C T I O N */}
+                <div className="mb-6 md:mb-4 max-w-7xl px:4 lg:px-10 md:px-14 mx-auto flex items-center ">
+                  <button
+                    type="button"
+                    onClick={goToPrevReview}
+                    className="p-2 rounded border border-stone-600 hover:bg-stone-900 md:-mt-25 "
+                  >
+                    <ArrowLeft className="" />
+                  </button>
+
+                  <div className="min-h-24 max-w-7xl mx-auto pt-20 px-8 sm:px-12 lg:px-16 items-center justify-center">
+                    <h3 className="text-sm text-gray-400 mb-1 relative">
+                      Reviews
+                    </h3>
+                    <div className="md:relative h-60 w-75 overflow-hidden">
+                      {activeReview ? (
+                        <div className="absolute ">
+                          <h4 className="text-lg font-semibold">
+                            {activeReview.source}
+                          </h4>
+                          <p className="text-base md:text-base font-bold">
+                            {activeReview.quote}
+                          </p>
+                          <p className="text-xs text-gray-400 mt-2">
+                            {reviewIndex + 1} / {reviewCount}
+                          </p>
+                        </div>
+                      ) : (
+                        <p className="text-base text-gray-300">
+                          Inga reviews tillgängliga.
+                        </p>
+                      )}
+                    </div>
+                  </div>
+
+                  <button
+                    type="button"
+                    onClick={goToNextReview}
+                    className="p-2 rounded border border-stone-600 hover:bg-stone-900 md:-mt-25"
+                  >
+                    <ArrowRight size={20} />
+                  </button>
+                </div>
+              </div>
+            </section>
+          ) : (
+            <section
+              className={`flex flex-col 
+              bg-stone-950
+              pt-2.5 pb-2.5 rounded-lg mb-16 px-2 sm:px-4 max-w-7xl mx-auto`}
+            >
+              <div className="flex justify-center pt-5 pb-5">
+                <h2 className="text-2xl font-bold">Välj en visning</h2>
+              </div>
+              <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6 p-6 justify-center items-center ">
+                {showingsPerDate?.map(({ date, showings }) => (
+                  <article
+                    key={date}
+                    className="border rounded-xl border-stone-500 p-4 bg-black text-center place-self-center w-full max-w-xs mx-auto"
+                  >
+                    <div className="flex flex-col w-full justify-center items-center pb-2 pt-2 text-center">
+                      <h2 className="font-medium">
+                        {new Date(date).toLocaleDateString("sv-SE", {
+                          weekday: "long",
+                        })}
+                      </h2>
+                      <h3 className="font-extralight text-stone-300">
+                        {new Date(date).getDate()}/
+                        {new Date(date).getMonth() + 1}
+                      </h3>
+                    </div>
+                    <div className="flex justify-center">
+                      <hr className="text-stone-700 w-4/5 " />
+                    </div>
+                    {showings.map(({ showingId, time, name }: any) => (
+                      <div key={showingId} className="flex flex-col gap-2 p-5">
+                        <Link
+                          to={`/boka/${showingId}`}
+                          className="bg-stone-950 border rounded-xl border-stone-600 pt-3 pb-3 hover:bg-stone-800 transition-ease-in-out duration-300"
+                        >
+                          <p>{time.toString().slice(0, 5)}</p>
+                          <p>{name}</p>
+                        </Link>
+                      </div>
+                    ))}
+                  </article>
+                ))}
+              </div>
+            </section>
+          )}
+
+          {/* T R A I L E R   S E C T I O N */}
+          {showTrailer && (
+            <div
+              className="fixed inset-0 z-50 flex items-center justify-center backdrop-blur-xs "
+              onClick={() => setShowTrailer(false)}
+            >
+              <div className="flex flex-col md:w-148.25 md:h-100 justify-center items-center text-white px-4 py-3 rounded-lg ">
+                <button
+                  className="flex items-center gap-2 px-4 py-1.5 bg-red-800 text-white rounded transition-colors self-end mb-2"
+                  onClick={() => setShowTrailer(false)}
+                >
+                  <p className="font-medium">Stäng trailer</p>
+                </button>
+                {details?.youtube && (
+                  <iframe
+                    src={`https://www.youtube.com/embed/${details.youtube}?autoplay=1`}
+                    title={`${details.title} Trailer`}
+                    className="w-full h-full border-0 mb-4 rounded-lg"
+                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                    allowFullScreen
+                  ></iframe>
+                )}
+              </div>
+            </div>
+          )}
+        </>
+      );
+    } else if (viewing == "desktop") {
+      return (
+        <>
+          <div className="mt-20 flex flex-row">
+            <Link
+              className="flex row items-center w-170"
+              to={HomePage.route.path}
+            >
+              <MoveLeft className="mx-5" />
+              Back to Home Page
+            </Link>
+            <h1 className="flex items-center bg-stone-800  p-5 ml-10 scale-120 w-full rounded-l-2xl h-10">
+              {details?.title}
+            </h1>
+          </div>
           <section className={switchSections ? "md:hi" : ""}>
             <div className="md:mt-10 flex flex-col justify-center px-4 bg-[url('/cinema.webp')] bg-no-repeat bg-center ">
               <div
@@ -172,7 +384,7 @@ export default function ShowingsPage() {
                   </div>
                   <div className="mb-6  text-base md:text-xl font-medium">
                     <h3 className="text-sm text-gray-400 mb-1">Regissör</h3>
-                    <h5>{details.director}</h5>
+                    <h5>{details?.director}</h5>
                   </div>
                   <div className="mb-6 md:mb-0">
                     <h3 className="text-sm text-gray-400 mb-1">Skådespelare</h3>
@@ -238,7 +450,6 @@ export default function ShowingsPage() {
               </div>
             </div>
           </section>
-        ) : (
           <section
             className={`flex flex-col 
               bg-stone-950
@@ -281,35 +492,34 @@ export default function ShowingsPage() {
               ))}
             </div>
           </section>
-        )}
-
-        {/* T R A I L E R   S E C T I O N */}
-        {showTrailer && (
-          <div
-            className="fixed inset-0 z-50 flex items-center justify-center backdrop-blur-xs "
-            onClick={() => setShowTrailer(false)}
-          >
-            <div className="flex flex-col md:w-148.25 md:h-100 justify-center items-center text-white px-4 py-3 rounded-lg ">
-              <button
-                className="flex items-center gap-2 px-4 py-1.5 bg-red-800 text-white rounded transition-colors self-end mb-2"
-                onClick={() => setShowTrailer(false)}
-              >
-                <p className="font-medium">Stäng trailer</p>
-              </button>
-              {details?.youtube && (
-                <iframe
-                  src={`https://www.youtube.com/embed/${details.youtube}?autoplay=1`}
-                  title={`${details.title} Trailer`}
-                  className="w-full h-full border-0 mb-4 rounded-lg"
-                  allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-                  allowFullScreen
-                ></iframe>
-              )}
+          {/* T R A I L E R   S E C T I O N */}
+          {showTrailer && (
+            <div
+              className="fixed inset-0 z-50 flex items-center justify-center backdrop-blur-xs "
+              onClick={() => setShowTrailer(false)}
+            >
+              <div className="flex flex-col md:w-148.25 md:h-100 justify-center items-center text-white px-4 py-3 rounded-lg ">
+                <button
+                  className="flex items-center gap-2 px-4 py-1.5 bg-red-800 text-white rounded transition-colors self-end mb-2"
+                  onClick={() => setShowTrailer(false)}
+                >
+                  <p className="font-medium">Stäng trailer</p>
+                </button>
+                {details?.youtube && (
+                  <iframe
+                    src={`https://www.youtube.com/embed/${details.youtube}?autoplay=1`}
+                    title={`${details.title} Trailer`}
+                    className="w-full h-full border-0 mb-4 rounded-lg"
+                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                    allowFullScreen
+                  ></iframe>
+                )}
+              </div>
             </div>
-          </div>
-        )}
-      </>
-    );
+          )}
+        </>
+      );
+    }
   } else {
     return <NotFoundPage />;
   }
