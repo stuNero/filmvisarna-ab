58f3dd0a7223caed36015bd38d49d2e1e56fd7f0
jacoblasse <jacoblasse4@gmail.com>
2026-03-16 10:44:42 +0100

    LocaleDetailsPage added background

--- src/pages/LocaleDetailsPage.tsx
+++ src/pages/LocaleDetailsPage.tsx
@@ -6,13 +6,13 @@ LocaleDetailsPage.route = {
 export default function LocaleDetailsPage() {
   return (
     <>
-      <div
-        className="                        top-0 bottom-0 left-0 h-[75vh]
-                        content-center justify-center"
-      >
-        <h2>Detta är en informationssida om våra salar och lokaler</h2>
-        <p></p>
-      </div>
+      <section>
+        <div className="min-h-screen flex flex-col justify-center px-4 bg-[url('https://images.unsplash.com/photo-1524985069026-dd778a71c7b4?auto=format&fit=crop&q=80&w=1920')] bg-no-repeat bg-center ">
+          <div className="container mx-auto">
+            </div>
+          </div>
+        
+      </section>
     </>
   );
 }
