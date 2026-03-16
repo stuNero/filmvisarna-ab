fdecb6598df17c69f3095a1abfd452ddcd956060
jacoblasse <jacoblasse4@gmail.com>
2026-03-16 10:58:52 +0100

    Changed picture size LocaleDetailsPage for better design

--- src/pages/LocaleDetailsPage.tsx
+++ src/pages/LocaleDetailsPage.tsx
@@ -7,10 +7,16 @@ export default function LocaleDetailsPage() {
   return (
     <>
       <section>
-        <div className="min-h-screen flex flex-col justify-center px-4 bg-[url('https://images.unsplash.com/photo-1524985069026-dd778a71c7b4?auto=format&fit=crop&q=80&w=1920')] bg-no-repeat bg-center ">
-          <div className="container mx-auto">
-            </div>
-          </div>
+      
+      {/* Hero Section */}
+      <div className="relative h-96 mb-16 overflow-hidden">
+        <div className="absolute inset-0 bg-gradient-to-b from-transparent via-black/50 to-black z-10" />
+        <img 
+          src="https://images.unsplash.com/photo-1524985069026-dd778a71c7b4?auto=format&fit=crop&q=80&w=1920"
+          alt="Cinema Interior"
+          className="w-full h-full object-cover"
+        />
+      </div>
         
       </section>
     </>
