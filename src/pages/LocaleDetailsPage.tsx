1b535561b549b3eeaffb3012f9d675584803fda3
jacoblasse <jacoblasse4@gmail.com>
2026-03-16 11:34:09 +0100

    Add section for "Våra Salar" in LocaleDetailsPage

--- src/pages/LocaleDetailsPage.tsx
+++ src/pages/LocaleDetailsPage.tsx
@@ -27,6 +27,17 @@ export default function LocaleDetailsPage() {
       </div>
         
       </section>
+
+      <section>
+        <div className="max-w-4xl mx-auto px-4 py-8">
+          <div>
+            <h2 className="text-3xl font-bold mb-4">Våra Salar</h2>
+          </div>
+          <div className="">
+
+          </div>
+        </div>
+      </section>
     </>
   );
 }
