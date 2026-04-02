33a28baa8b54bed466792fbe2161eb58d3c2dbe8
stuNero <max.vemic@student.nbi-handelsakademin.se>
2026-04-02 15:27:59 +0200

    fix typo

--- src/pages/MovieDetailShowingsPage.tsx
+++ src/pages/MovieDetailShowingsPage.tsx
@@ -20,7 +20,7 @@ export default function MovieDetailShowingsPage() {
 
   if (details?.id === movieId) {
     return <>
-      <div className="min-h-screen flex flex-col justify-center px-4 bg-[url('/cinema.webp')] bg-no-repeat bg-center ">
+      <div className="min-h-screen flex flex-col justify-center px-4 bg-[url('/Cinema.webp')] bg-no-repeat bg-center ">
         <div className="flex flex-col md:flex-row md:items-start">
           <img src={details?.coverImage} alt={details?.title} className="w-full md:max-w-sm max-h-137.5 p-5 rounded-3xl object-cover" />
           <div className="md:ml-8 backdrop-blur-xs">
